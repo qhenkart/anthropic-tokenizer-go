ec8e1a9d8e8bfe1f7b5ada6f75e7ee127dcc1672
Quest Henkart <qhenkart@gmail.com>
2023-10-09 20:54:58 -0400

    init

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/qhenkart/anthropic-tokenizer-go
+
+go 1.21.1
+
+require github.com/pkoukk/tiktoken-go v0.1.6
+
+require (
+	github.com/dlclark/regexp2 v1.10.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+)
---
+++ main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func TestStuff(t *testing.T) {
+
+	t.Run("small_text", func(t *testing.T) {
+		if tokens := countTokens("hello world!"); tokens != 3 {
+			t.Errorf("expected 3 got %d", tokens)
+		}
+	})
+
+	t.Run("small_normalize", func(t *testing.T) {
+		if tokens := countTokens("™"); tokens != 1 {
+			t.Errorf("expected 1 got %d", tokens)
+		}
+
+		if tokens := countTokens("ϰ"); tokens != 1 {
+			t.Errorf("expected 1 got %d", tokens)
+		}
+
+	})
+
+	t.Run("allows_special_tokens", func(t *testing.T) {
+		if tokens := countTokens("™"); tokens != 1 {
+			t.Errorf("expected 1 got %d", tokens)
+		}
+
+		if tokens := countTokens("<EOT>"); tokens != 1 {
+			t.Errorf("expected 1 got %d", tokens)
+		}
+	})
+}
